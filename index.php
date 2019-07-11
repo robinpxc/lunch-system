4ed6559c8ee778e3067792678222f460ac45d807
robin.pan <robinpxc@qq.com>
2019-07-11 12:55:16 +0800

    add dependencies bootstrap, jquery, proper

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="zh">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="third-party/bootstrap-4.3.1-dist/css/bootstrap.min.css">
         <title>吃啥？</title>
     </head>
 
     <body>
+      <script src="third-party/popper.min.js"></script>
+      <script src="third-party/jquery-3.4.1.min.js"></script>
+      <script src="third-party/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
     </body>
 </html>
