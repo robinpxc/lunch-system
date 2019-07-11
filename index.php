34590868956c509afaf7059ce4511c2df178e885
robin.pan <robinpxc@qq.com>
2019-07-11 18:01:15 +0800

    Working on login page, add some assets

--- index.php
+++ index.php
@@ -1,17 +1,47 @@
-<!DOCTYPE html>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/icons/rice_32x32.ico">
 
-<html lang="zh">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>中午饭吃啥咩？</title>
 
-        <link rel="stylesheet" href="third-party/bootstrap-4.3.1-dist/css/bootstrap.min.css">
-        <title>吃啥？</title>
-    </head>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="third-party/bootstrap-4.3.1-dist/css/bootstrap.min.css">
 
-    <body>
-      <script src="third-party/popper.min.js"></script>
-      <script src="third-party/jquery-3.4.1.min.js"></script>
-      <script src="third-party/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
-    </body>
+    <!-- Custom styles for this template -->
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin">
+      <div class="title-container mb-4">
+        <img class="food_icon" src="assets/images/food.png">
+        <h1 class="h3 font-weight-normal">中午吃啥</h1>
+      </div>
+
+      <div class="title-info-container">
+        <p>**吃饭是一件轻松愉快的事情</p>
+      </div>
+
+      <label for="inputUsername" class="sr-only">Username</label>
+      <input type="text" id="inputUsername" class="form-control" placeholder="饭友编号/你的全名" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="吃饭密码" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> 记住我
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">
+        <img src="assets/icons/sausage_32x32.png">&nbsp;&nbsp;进入菜谱
+      </button>
+
+      <p class="mt-5 mb-3 text-muted">李团长说过：打仗归打仗，饭还是要吃的</p>
+
+    </form>
+  </body>
 </html>
