a144913add68ee205e7258f76055527c0feeadd2
robin.pan <robinpxc@qq.com>
2019-07-12 10:25:27 +0800

    Finished login page UI design.

--- index.php
+++ index.php
@@ -12,35 +12,53 @@
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="third-party/bootstrap-4.3.1-dist/css/bootstrap.min.css">
 
-    <!-- Custom styles for this template -->
+    <!-- Custom styles-->
     <link href="css/main.css" rel="stylesheet">
   </head>
 
   <body class="text-center">
     <form class="form-signin">
       <div class="title-container mb-4">
-        <img class="food_icon" src="assets/images/food.png">
-        <h1 class="h3 font-weight-normal">中午吃啥</h1>
+        <img class="food_icon" src="assets/images/food.png" alt="Left food icon" description="Food icon left">
+        <h1 class="h3 font-weight-normal">中午吃啥?</h1>
+        <img class="food_icon" src="assets/images/food-inverse.png" alt="Right food icon" description="Food icon right">
       </div>
 
       <div class="title-info-container">
-        <p>**吃饭是一件轻松愉快的事情</p>
+        <p>吃饭是一件轻松愉快的事</p>
       </div>
 
       <label for="inputUsername" class="sr-only">Username</label>
-      <input type="text" id="inputUsername" class="form-control" placeholder="饭友编号/你的全名" required autofocus>
+      <input type="text" id="inputUsername" class="form-control" placeholder="饭友编号/你的全名" required>
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="吃饭密码" required>
-      <div class="checkbox mb-3">
+      <input type="password" id="inputPassword" class="form-control mb-3" placeholder="吃饭密码" required>
+      <!-- <div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me"> 记住我
         </label>
-      </div>
+      </div> -->
       <button class="btn btn-lg btn-primary btn-block" type="submit">
-        <img src="assets/icons/sausage_32x32.png">&nbsp;&nbsp;进入菜谱
+        <img src="assets/icons/donut_32x32.png" alt="Donut icon" description="Login button icon">&nbsp;进入系统
       </button>
 
-      <p class="mt-5 mb-3 text-muted">李团长说过：打仗归打仗，饭还是要吃的</p>
+      <div class="quotes">
+        <div class="mt-4 mb-1 text-muted">
+          <embed src="assets/svgs/food01.svg" type="image/svg+xml" />
+          <p>李团长说过：打仗归打仗，饭还是要吃的</p>
+        </div>
+        <div class="mb-1 text-muted">
+          <embed src="assets/svgs/food02.svg" type="image/svg+xml" />
+          <p>王者以民为天，而民以食为天</p>
+        </div>
+        <div class="mb-1 text-muted">
+          <embed src="assets/svgs/food03.svg" type="image/svg+xml" />
+          <p>让食物成为你的药品，药品应该是你的食物</p>
+        </div>
+        <div class="mb-1 text-muted">
+          <embed src="assets/svgs/food04.svg" type="image/svg+xml" />
+          <p>食之过饱，心灵无法容忍躯体；食不果腹，躯体同样无法支撑心灵</p>
+        </div>
+      </div>
 
     </form>
   </body>
