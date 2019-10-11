5678017024257ac217c101238df70896631cd226
robin.pan <robinpxc@qq.com>
2019-10-11 22:50:43 +0800

    WIP - Add update menu related functions.

---
+++ php/functions/fetch-menu.php
@@ -0,0 +1,15 @@
+<?php
+include('../Common/config.php');
+
+echo json_decode(fetchMenuData());
+
+function fetchMenuData() {
+  global $mysqlConnection;
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $date = $_POST['date'];
+    $sql_fetch_menu = "SELECT `menu-list` FROM `lunch-menu` WHERE `date` = '$date'";
+    $result = mysqli_query($mysqlConnection, $sql_fetch_menu);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    return $row['menu-list'];
+  }
+}
