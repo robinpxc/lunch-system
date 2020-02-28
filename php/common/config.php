d984011b3b6088da8326f99315425bfad9302048
robin.pan <robinpxc@qq.com>
2020-02-28 15:20:09 +0800

    Update password for scurity, add DB_PORT in config file

--- php/common/config.php
+++ php/common/config.php
@@ -2,9 +2,10 @@
 require('PasswordHash.php');
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'xcb123');
+define('DB_PASSWORD', '(Xcblunch)98266');
 define('DB_DATABASE', 'user_db');
-$mysqlConnection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+define('DB_PORT', '3308');
+$mysqlConnection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE,DB_PORT);
 mysqli_set_charset($mysqlConnection, "utf8");
 session_start();
 ?>
