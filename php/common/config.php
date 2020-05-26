fe14af90944d83cc4c93109ec0e329cd77266789
robin.pan <robinpxc@qq.com>
2020-05-26 11:36:06 +0800

    WIP - Trying to solve a connection bug.

--- php/common/config.php
+++ php/common/config.php
@@ -1,11 +1,15 @@
 <?php
 require('PasswordHash.php');
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '(Xcblunch)98266');
 define('DB_DATABASE', 'user_db');
 define('DB_PORT', '3308');
-$mysqlConnection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE,DB_PORT);
-mysqli_set_charset($mysqlConnection, "utf8");
+//$mysqlConnection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE,DB_PORT);
+//  $data = mysqli_set_charset($mysqlConnection, "utf8");
+//  if (!$data) {
+//    printf("Error: %s\n", mysqli_error($mysqlConnection));
+//    exit();
+//  }
 session_start();
 ?>
